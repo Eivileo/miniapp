235945810db131d68dd1f1efaaa8708975f94d51
longen0707 <949729154@qq.com>
2019-07-27 20:06:47 +0800

    first commit

---
+++ pages/shadiao/shadiao.js
@@ -0,0 +1,77 @@
+// pages/shadiao/shadiao.js
+// Page({
+
+//   /**
+//    * 页面的初始数据
+//    */
+//   data: {
+
+//   },
+
+//   /**
+//    * 生命周期函数--监听页面加载
+//    */
+//   onLoad: function (options) {
+
+//   },
+
+//   /**
+//    * 生命周期函数--监听页面初次渲染完成
+//    */
+//   onReady: function () {
+
+//   },
+
+//   /**
+//    * 生命周期函数--监听页面显示
+//    */
+//   onShow: function () {
+
+//   },
+
+//   /**
+//    * 生命周期函数--监听页面隐藏
+//    */
+//   onHide: function () {
+
+//   },
+
+//   /**
+//    * 生命周期函数--监听页面卸载
+//    */
+//   onUnload: function () {
+
+//   },
+
+//   /**
+//    * 页面相关事件处理函数--监听用户下拉动作
+//    */
+//   onPullDownRefresh: function () {
+
+//   },
+
+//   /**
+//    * 页面上拉触底事件的处理函数
+//    */
+//   onReachBottom: function () {
+
+//   },
+
+//   /**
+//    * 用户点击右上角分享
+//    */
+//   onShareAppMessage: function () {
+
+//   }
+// })
+const navigationBarHeight = (getApp().statusBarHeight + 44) + 'px'
+
+Page({
+  data: {
+    navigationBarTitle: 'Custom Navigation Bar',
+    navigationBarHeight
+  },
+  onLoad: function () {
+
+  },
+})
